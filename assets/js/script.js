4399bc906b672e124ec7bb1e246172168eca5a28
Jake <xxjakexx14@gmail.com>
2021-09-17 15:18:20 -0400

    added question data to store questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var startButton = document.getElementById("start-quiz");
 
 function buildQuiz() {
   clearContainer("info");
-  
+
 }
 
 function clearContainer(containerIdName) {
@@ -19,5 +19,32 @@ function clearContainer(containerIdName) {
       break;
   }
 }
+
+var questionData = [
+  {
+    question: "Question 1",
+    answers: ["1", "2", "3", "4"],
+  },
+  {
+    question: "Question 2",
+    answers: ["3", "4", "5", "6"],
+  },
+  {
+    question: "Question 3",
+    answers: ["7", "8", "9", "10"],
+  },
+  {
+    question: "Question 4",
+    answers: ["11", "12", "13", "14"],
+  },
+  {
+    question: "Question 5",
+    answers: ["15", "16", "17", "18"],
+  },
+  {
+    question: "Question 6",
+    answers: ["19", "20", "21", "22"],
+  },
+];
 
 startButton.addEventListener("click", buildQuiz);
